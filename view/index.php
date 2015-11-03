3809b2a50a8d7dcfe0a5f360143d50cf6e69af9b
guillaume <gui.nouhaud@gmail.com>
2015-11-03 10:43:55 +0100

    récupérer des données de la base

--- view/index.php
+++ view/index.php
@@ -3,6 +3,7 @@
  $bdd = new PDO('mysql:host=localhost;dbname=e-ticket;charset=utf8', 'root', '');
  				$reponse = $bdd->query('SELECT * FROM account');
  				$donnees = $reponse->fetchAll();
-
-		echo 'mon pseudo est'.$pseudo;
+		for ($p=0; $p < count($donnees) ; $p++) {
+		echo 'mon pseudo est'.$donnees[$p]['pseudo'];
+    }
   ?>
