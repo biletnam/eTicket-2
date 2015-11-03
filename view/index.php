dc2a66e2616ef22c47593514133c90d1d136317b
guillaume <gui.nouhaud@gmail.com>
2015-11-03 10:28:53 +0100

    se connecter a la BDD

---
+++ view/index.php
@@ -0,0 +1,8 @@
+<?php
+
+ $bdd = new PDO('mysql:host=localhost;dbname=e-ticket;charset=utf8', 'root', '');
+ 				$reponse = $bdd->query('SELECT * FROM account');
+ 				$donnees = $reponse->fetchAll();
+
+		echo 'mon pseudo est.'$pseudo;
+  ?>
