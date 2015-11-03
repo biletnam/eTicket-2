86b1d3c8e6105785b5babb21714d30eafa755142
Fabien Montillot <fabmlt@MacBook-Pro-de-Fabien.local>
2015-11-03 14:35:49 +0100

    Product - Simple Product

--- single-product.php
+++ single-product.php
@@ -2,10 +2,10 @@
 
 require_once __DIR__.'/vendor/autoload.php';
 require_once __DIR__.'/controller/BaseCtrl.php';
-require_once __DIR__.'/controller/ProductsCtrl.php';
+require_once __DIR__.'/controller/SingleProductCtrl.php';
 
 
-$ctrl   = new ProductsCtrl();
+$ctrl   = new SingleProductCtrl();
 $render = $ctrl->index();
 
 echo $render;
