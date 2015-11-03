db6baa61279e2a39863f2d174184c20f37e9640a
Thomas <gueguen.thomas92@gmail.com>
2015-11-03 15:18:20 +0100

    signinCtrl / user.php update

--- model/bdd.php
+++ model/bdd.php
@@ -14,7 +14,7 @@ class Bdd{
     try{
       $strConnexion = 'mysql:host=localhost;dbname=e-ticket';
       $arrExtraParam= array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
-      $this->pdo = new PDO($strConnexion, 'root', '', $arrExtraParam);
+      $this->pdo = new PDO($strConnexion, 'root', 'root', $arrExtraParam);
       $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
     catch(PDOException $e) {
@@ -24,6 +24,12 @@ class Bdd{
   }
 
   function getPdo(){return $this->pdo;}
+  function donnees(){
+    $bdd = new PDO('mysql:host=localhost;dbname=e-ticket;charset=utf8', 'root', 'root');
+				$reponse = $bdd->query('SELECT * FROM account');
+				$donnees = $reponse->fetchAll();
+        return $donnees;
+  }
 
 }
 
