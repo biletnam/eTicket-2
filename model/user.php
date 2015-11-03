f6484370f5f68f6f0b97293e5031c6d621f04343
berelindis <romain.bellande@orange.fr>
2015-11-03 11:51:31 +0100

    user.php

--- model/user.php
+++ model/user.php
@@ -1,13 +1,19 @@
 <?php
+
+require_once('bdd.php');
+
 class User{
   private $id;
   private $pseudo;
   private $password;
   private $email;
+  private $bdd;
 
-  function __construct(){
+  function __construct(Bdd $bdd){
+    $this->bdd = $bdd;
     $ctp = func_num_args();
     $args = func_get_args();
+
     switch($ctp)
     {
       case 2:
@@ -25,6 +31,46 @@ class User{
   function getId(){return $this->id;}
   function getPseudo{return $this->pseudo;}
   function getEmail{return $this->email;}
+
+  function verif_exist()
+  {
+    $pdo = $this->bdd->getPdo();
+    $login = $this->login;
+    $password = $this->password;
+    $query = "SELECT id
+    FROM account
+    WHERE pseudo = ?";
+    $prep = $pdo->prepare($query);
+    $prep->bindValue(1,$this->pseudo,PDO::PARAM_STR);
+    $prep->execute();
+    if($prep->rowCount() == 1)
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+    $bdd->deconnexion();
+  }
+
+  function addUser()
+  {
+    $pdo = $this->bdd->getPdo();
+
+
+    $query = "INSERT INTO account (pseudo,password,email) VALUES(?,?,?)";
+    $prep = $pdo->prepare($query);
+    $prep->bindValue(1,$this->pseudo,PDO::PARAM_STR);
+    $prep->bindValue(2,$this->password,PDO::PARAM_STR);
+    $prep->bindValue(3,$this->email,PDO::PARAM_STR);
+
+    $prep->execute();
+
+    $bdd->deconnexion();
+
+
+  }
 
 
 
